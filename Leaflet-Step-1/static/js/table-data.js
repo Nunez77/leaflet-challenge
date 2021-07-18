7122398ffd28ada6afc9156741865f7b750656e1
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-07-17 21:49:22 -0500

    trial

--- Leaflet-Step-1/static/js/table-data.js
+++ Leaflet-Step-1/static/js/table-data.js
@@ -11,57 +11,48 @@
     // All Earthquakes -> Past hour
     // url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
      
+    // Grab data from the jason file and store it in a variable
      d3.json(url).then((data) => {
-     
-         // Store the imported data to a variable
-         var EarthquakesData = data;
+         var QuakesData = data;
          // Print the data
-         console.log(EarthquakesData);
-     
-         // Print the object keys
-         console.log(Object.keys(EarthquakesData));
-     
-         // Get the date that the data was generated
-         var dataDate = new Date(EarthquakesData.metadata.generated);
+         console.log(QuakesData);
+         // Print object keys
+         console.log(Object.keys(QuakesData));
+         // Graba datadate
+         var dataDate = new Date(QuakesData.metadata.generated);
          console.log(`Data retrieved at: ${dataDate}`);
+        // Number of records
+         console.log(`Number of records: ${QuakesData.metadata.count}`);
+         // Magnitudes
+         console.log(QuakesData.features[0].properties.mag);
+         // Times
+         console.log(new Date(QuakesData.features[0].properties.time));
+         // Latitudet
+         console.log(QuakesData.features[0].geometry.coordinates[0]);
+         // Longitude
+         console.log(QuakesData.features[0].geometry.coordinates[1]);
+         // Depths
+         console.log(QuakesData.features[0].geometry.coordinates[2]);
      
-         // Number of data points on the data set
-         console.log(`Number of records: ${EarthquakesData.metadata.count}`);
-     
-         // Earthquakes magnitude
-         console.log(EarthquakesData.features[0].properties.mag);
-     
-         // Earthquakes time
-         console.log(new Date(EarthquakesData.features[0].properties.time));
-     
-         // Earthquakes lat
-         console.log(EarthquakesData.features[0].geometry.coordinates[0]);
-     
-         // Earthquakes lon
-         console.log(EarthquakesData.features[0].geometry.coordinates[1]);
-     
-         // Earthquakes depth
-         console.log(EarthquakesData.features[0].geometry.coordinates[2]);
-     
-     
-         // Create a object list with the target data columns
+         // Create a object list with the chosen data
          var cleanData = [];
-         for (var i = 0; i < EarthquakesData.features.length; i++) {
-             var time = new Date(EarthquakesData.features[i].properties.time);
+         for (var i = 0; i < QuakesData.features.length; i++) {
+             var time = new Date(QuakesData.features[i].properties.time);
              cleanData.push({
                  "time": time.toLocaleTimeString("en-US", options),
-                 "title": EarthquakesData.features[i].properties.title,
-                 "url": EarthquakesData.features[i].properties.url,
-                 "lat": EarthquakesData.features[i].geometry.coordinates[0],
-                 "lon": EarthquakesData.features[i].geometry.coordinates[1],
-                 "mag": EarthquakesData.features[i].properties.mag,
-                 "depth": EarthquakesData.features[i].geometry.coordinates[2]
+                 "title": QuakesData.features[i].properties.title,
+                 "url": QuakesData.features[i].properties.url,
+                 "lat": QuakesData.features[i].geometry.coordinates[0],
+                 "lon": QuakesData.features[i].geometry.coordinates[1],
+                 "mag": QuakesData.features[i].properties.mag,
+                 "depth": QuakesData.features[i].geometry.coordinates[2]
              });
          };
      
          console.log(cleanData);
      
          var index = 0;
+         
          // Insert a table
          d3.select("table")
              .selectAll("tr")
